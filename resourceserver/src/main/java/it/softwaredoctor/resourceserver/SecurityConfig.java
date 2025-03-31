74125b028adde24ba7702c9cb3b7bb721c704700
ICE0160 <andrea.italiano.ext@infocert.it>
2025-03-31 13:51:53 +0200

    jwt decoder

--- resourceserver/src/main/java/it/softwaredoctor/resourceserver/SecurityConfig.java
+++ resourceserver/src/main/java/it/softwaredoctor/resourceserver/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,20 +24,12 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                        .jwt(jwt -> jwt.decoder(jwtDecoder()))
                 );
         return http.build();
     }
 
-    @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
-        return jwtAuthenticationConverter;
-    }
-
+//    JwtDecoder è configurato per utilizzare un URI del JWK Set che recupera la chiave pubblica dall' endpoint /oauth2/jwks per verificare i token JWT
     @Bean
     public JwtDecoder jwtDecoder() {
         try {
